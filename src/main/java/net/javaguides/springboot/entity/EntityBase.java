2fb69bd51106db335bc02e791f64966cdf9537b2
MOHAN KSHIRSAGAR <57714076+MakMohanK@users.noreply.github.com>
2024-06-11 12:20:04 +0530

    Create EntityBase.java

---
+++ src/main/java/net/javaguides/springboot/entity/EntityBase.java
@@ -0,0 +1,12 @@
+package net.javaguides.springboot.entity;
+
+public interface EntityBase extends java.io.Serializable {
+
+    /**
+     * The Database ID for this object.
+     * 
+     * @return the ID
+     */
+    public Long getId();
+  
+  }
